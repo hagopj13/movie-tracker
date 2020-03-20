e92c215738e7d09bc6c8b9d9ff4d8db362e02cfa
Hagop Jamkojian <hagopj13@gmail.com>
2020-03-20 23:37:15 +0100

    Add styling for header

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,6 +1,27 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
+import Button from '@material-ui/core/Button';
+
 import './header.scss';
 
-const Header = () => <div className="header">Header</div>;
+const Header = () => (
+  <div className="header">
+    <div>Search</div>
+    <div className="nav-links-section">
+      <NavLink exact to="/" className="nav-link" activeClassName="selected-nav-link">
+        Discover
+      </NavLink>
+      <NavLink to="/profile" className="nav-link" activeClassName="selected-nav-link">
+        Profile
+      </NavLink>
+    </div>
+    <div>
+      <Button variant="contained">Signup</Button>
+      <Button variant="contained" color="primary">
+        Login
+      </Button>
+    </div>
+  </div>
+);
 
 export default Header;
