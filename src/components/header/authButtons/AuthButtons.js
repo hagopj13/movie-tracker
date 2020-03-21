5581e87cfb05feb5cfadc86b4edc63701efcbbbc
Hagop Jamkojian <hagopj13@gmail.com>
2020-03-22 00:38:29 +0100

    Rename buttons main class to root

--- src/components/header/authButtons/AuthButtons.js
+++ src/components/header/authButtons/AuthButtons.js
@@ -3,7 +3,7 @@ import { makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 
 const useStyles = makeStyles((theme) => ({
-  buttons: {
+  root: {
     '& > *': {
       textTransform: 'none',
       margin: theme.spacing(0.5),
@@ -15,7 +15,7 @@ const AuthButtons = () => {
   const classes = useStyles();
 
   return (
-    <div className={classes.buttons}>
+    <div className={classes.root}>
       <Button variant="contained">Signup</Button>
       <Button variant="contained" color="primary">
         Login
