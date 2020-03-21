1e2c1729e3ac51b7df4a3f9e63e3da2d15498b8e
Hagop Jamkojian <hagopj13@gmail.com>
2020-03-21 23:05:45 +0100

    Change nav button styles

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,27 +1,42 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import { makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 
 import './header.scss';
 
-const Header = () => (
-  <div className="header">
-    <div>Search</div>
-    <div className="nav-links-section">
-      <NavLink exact to="/" className="nav-link" activeClassName="selected-nav-link">
-        Discover
-      </NavLink>
-      <NavLink to="/profile" className="nav-link" activeClassName="selected-nav-link">
-        Profile
-      </NavLink>
+const useStyles = makeStyles((theme) => ({
+  buttons: {
+    '& > *': {
+      textTransform: 'none',
+      margin: theme.spacing(0.5),
+    },
+  },
+}));
+
+const Header = () => {
+  const classes = useStyles();
+
+  return (
+    <div className="header">
+      <div>Search</div>
+      <div className="nav-section">
+        <NavLink exact to="/" className="nav-link" activeClassName="selected-nav-link">
+          Discover
+        </NavLink>
+        <NavLink to="/profile" className="nav-link" activeClassName="selected-nav-link">
+          Profile
+        </NavLink>
+      </div>
+      <div className={classes.buttons}>
+        <Button variant="contained">Signup</Button>
+        <Button variant="contained" color="primary">
+          Login
+        </Button>
+        <Button variant="contained">Logout</Button>
+      </div>
     </div>
-    <div>
-      <Button variant="contained">Signup</Button>
-      <Button variant="contained" color="primary">
-        Login
-      </Button>
-    </div>
-  </div>
-);
+  );
+};
 
 export default Header;
--- src/styles/muiTheme.js
+++ src/styles/muiTheme.js
@@ -1,3 +1,13 @@
 import { createMuiTheme } from '@material-ui/core';
 
-export default createMuiTheme({});
+export default createMuiTheme({
+  typography: {
+    htmlFontSize: 10,
+    fontSize: 12,
+  },
+  palette: {
+    primary: {
+      main: '#0e9d5f',
+    },
+  },
+});
