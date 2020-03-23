d7009c46f339fdffea2f3b2c26ee34e297ef64d3
Hagop Jamkojian <hagopj13@gmail.com>
2020-03-23 01:42:24 +0100

    Remove sign up feature

--- src/components/auth/authButtons/AuthButtons.js
+++ src/components/auth/authButtons/AuthButtons.js
@@ -3,7 +3,6 @@ import { makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 
 import LoginDialog from '../loginDialog/LoginDialog';
-import SignUpDialog from '../signUpDialog/SignUpDialog';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -16,24 +15,17 @@ const useStyles = makeStyles((theme) => ({
 
 const AuthButtons = () => {
   const classes = useStyles();
-  const [signUpOpen, setSignUpOpen] = useState(false);
   const [logInOpen, setLogInOpen] = useState(false);
 
-  const handleSignUpOpen = () => setSignUpOpen(true);
-  const handleSignUpClose = () => setSignUpOpen(false);
   const handleLoginOpen = () => setLogInOpen(true);
   const handleLoginClose = () => setLogInOpen(false);
 
   return (
     <div className={classes.root}>
-      <Button variant="contained" onClick={handleSignUpOpen}>
-        Sign Up
-      </Button>
       <Button variant="contained" color="primary" onClick={handleLoginOpen}>
         Login
       </Button>
       <Button variant="contained">Logout</Button>
-      <SignUpDialog open={signUpOpen} onClose={handleSignUpClose} />
       <LoginDialog open={logInOpen} onClose={handleLoginClose} />
     </div>
   );
