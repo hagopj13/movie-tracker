417355b269a23da541b707359b58202f0083b156
Hagop Jamkojian <hagopj13@gmail.com>
2020-03-23 02:09:25 +0100

    Add description to login with tmdb account

--- src/components/auth/loginDialog/LoginDialog.js
+++ src/components/auth/loginDialog/LoginDialog.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Dialog from '@material-ui/core/Dialog';
 import DialogContent from '@material-ui/core/DialogContent';
+import Typography from '@material-ui/core/Typography';
+import Link from '@material-ui/core/Link';
 
 import DialogTitle from '../../common/dialog/dialogTitle/DialogTitle';
 import LoginForm from './loginForm/LoginForm';
@@ -13,8 +15,12 @@ type Props = {
 
 const useStyles = makeStyles((theme) => ({
   content: {
-    padding: theme.spacing(2, 4, 1),
+    padding: theme.spacing(0.5, 5, 1),
     marginBottom: theme.spacing(2),
+  },
+  contentText: {
+    marginBottom: theme.spacing(3),
+    maxWidth: '45ch',
   },
 }));
 
@@ -32,6 +38,14 @@ const LoginDialog = (props: Props) => {
     <Dialog open={open} onClose={onClose} maxWidth={false}>
       <DialogTitle onClose={onClose}>Login</DialogTitle>
       <DialogContent className={classes.content}>
+        <Typography className={classes.contentText}>
+          This app gets its data from the TMDb APIs. To view your account information, login with
+          your TMDb credentials in the form below. To create one,&nbsp;
+          <Link target="_blank" href="https://www.themoviedb.org/account/signup" color="primary">
+            click here
+          </Link>
+          .
+        </Typography>
         <LoginForm onSubmit={handleSubmit} />
       </DialogContent>
     </Dialog>
