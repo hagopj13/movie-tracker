96c28df2b463fcd3d82487c2ed36d979f656ee9a
Hagop Jamkojian <hagopj13@gmail.com>
2020-04-16 00:48:38 +0200

    Add missing return statement

--- src/store/common/utils/typesGenerator.js
+++ src/store/common/utils/typesGenerator.js
@@ -1,7 +1,7 @@
 import typeGenerator from './typeGenerator';
 
 function typesGenerator<T = any>(namespace: string, types: any): T {
-  Object.keys(types).reduce((result, key) => {
+  return Object.keys(types).reduce((result, key) => {
     result[key] = typeGenerator(namespace, types[key]);
     return result;
   }, {});
