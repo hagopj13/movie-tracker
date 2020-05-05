0c2f20059eadc880e87399758844007b70c76466
Hagop Jamkojian <hagopj13@gmail.com>
2020-05-05 14:30:48 +0200

    Add api error loading and loaded state tests

--- src/store/config/__tests__/config.selectors.test.js
+++ src/store/config/__tests__/config.selectors.test.js
@@ -2,15 +2,12 @@ import configSelectors from 'store/config/config.selectors';
 import configState, { images as imagesConfig, genres } from 'store/fixtures/config';
 
 describe('Config selectors', () => {
-  const testState = {
-    config: configState,
-  };
-
   describe('createBackdropFullPathSelector', () => {
     it('should create a backdrop full path selector', () => {
       const path = 'test.png';
+      const currentState = { config: configState };
       const selectBackdropFullPath = configSelectors.createBackdropFullPathSelector(path);
-      const backdropFullPath = selectBackdropFullPath(testState);
+      const backdropFullPath = selectBackdropFullPath(currentState);
       expect(backdropFullPath).toBe(
         `${imagesConfig.baseImageUrl}${imagesConfig.backdropSize}${path}`,
       );
@@ -20,8 +17,9 @@ describe('Config selectors', () => {
   describe('createPosterFullPathSelector', () => {
     it('should create a poster full path selector', () => {
       const path = 'test.png';
+      const currentState = { config: configState };
       const selectPosterFullPath = configSelectors.createPosterFullPathSelector(path);
-      const posterFullPath = selectPosterFullPath(testState);
+      const posterFullPath = selectPosterFullPath(currentState);
       expect(posterFullPath).toBe(`${imagesConfig.baseImageUrl}${imagesConfig.posterSize}${path}`);
     });
   });
@@ -29,8 +27,9 @@ describe('Config selectors', () => {
   describe('createProfileFullPathSelector', () => {
     it('should create a profile full path selector', () => {
       const path = 'test.png';
+      const currentState = { config: configState };
       const selectProfileFullPath = configSelectors.createProfileFullPathSelector(path);
-      const profileFullPath = selectProfileFullPath(testState);
+      const profileFullPath = selectProfileFullPath(currentState);
       expect(profileFullPath).toBe(
         `${imagesConfig.baseImageUrl}${imagesConfig.profileSize}${path}`,
       );
@@ -39,7 +38,8 @@ describe('Config selectors', () => {
 
   describe('selectAllGenres', () => {
     it('should return all genres', () => {
-      const allGenres = configSelectors.selectAllGenres(testState);
+      const currentState = { config: configState };
+      const allGenres = configSelectors.selectAllGenres(currentState);
       expect(allGenres).toEqual(genres);
     });
   });
