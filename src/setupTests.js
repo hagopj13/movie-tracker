760eb0335a3b12e5839dc14e2caa533deb644a5f
Hagop Jamkojian <hagopj13@gmail.com>
2020-05-09 18:30:45 +0200

    Solve mutation observer not defined issue

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,7 +1,3 @@
-/* eslint-disable no-empty-function */
-/* eslint-disable class-methods-use-this */
-/* eslint-disable no-unused-vars */
-/* eslint-disable no-useless-constructor */
 // jest-dom adds custom jest matchers for asserting on DOM nodes.
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
@@ -9,13 +5,6 @@
 import '@testing-library/jest-dom/extend-expect';
 import Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
+import 'mutationobserver-shim';
 
 Enzyme.configure({ adapter: new Adapter() });
-
-global.MutationObserver = class {
-  constructor(callback) {}
-
-  disconnect() {}
-
-  observe(element, initObject) {}
-};
